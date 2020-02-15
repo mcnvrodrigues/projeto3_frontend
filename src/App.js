2de31439dc3abdf7ccdb68c1861b5f4da9dc889d
Marciano Vilela Rodrigues <mvr@br.ibm.com>
2020-02-15 13:48:47 -0300

    correcao

--- src/App.js
+++ src/App.js
@@ -4,14 +4,37 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
-
+import AppContext from './context/AppContext';
 import Signup from './components/auth/Signup';
+import Navbar from './components/Navbar';
+import Login from './components/auth/Login';
+import Confirmation from './components/auth/Confirmation';
+
+import AuthService from './components/auth/auth-service';
 
 class App extends Component {
 
   constructor(props){
     super(props)
     this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  fetchUser(){
+    console.log('fetchUser');
+    if( this.state.loggedInUser === null ){
+      this.service.loggedin()
+      .then(response =>{
+        this.setState({
+          loggedInUser:  response
+        }) 
+      })
+      .catch( err =>{
+        this.setState({
+          loggedInUser:  false
+        }) 
+      })
+    }
   }
 
   getTheUser= (userObj) => {
@@ -21,16 +44,58 @@ class App extends Component {
   } 
 
   render() {
+
+    const contextValues = {
+      state: this.state,
+      getUser: this.getTheUser,
+    }
+
+    // this.fetchUser();
+    
+    // if(this.state.loggedInUser){
+    // return (
+    //   <AppContext.Provider value = {contextValues}>
+    //   <div className="App">
+    //   {/* <Navbar userInSession={this.state.loggedInUser} /> */}
+    //     <Switch>
+    //       <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
+    //       <Route exact path='/' render={() => <Login getUser={this.getTheUser}/>}/>
+    //       <Route exact path='/auth/:confirmation' component={Confirmation}/>
+    //       {/* <Route exact path="/projects" component={ProjectList}/>
+    //       <Route exact path="/projects/:id" component={ProjectDetails} /> */}
+    //     </Switch>
+    //   </div>
+    //   </AppContext.Provider>
+    // );
+    // } else{
+    //   return(
+    //     <AppContext.Provider value = {contextValues}>
+    //       <div className="App">
+    //       {/* <Navbar userInSession={this.state.loggedInUser} /> */}
+    //         <Switch>
+    //           <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
+    //           <Route exact path='/auth/:confirmation' component={Confirmation}/>
+    //           {/* <Route exact path="/projects" component={ProjectList}/>
+    //           <Route exact path="/projects/:id" component={ProjectDetails} /> */}
+    //         </Switch>
+    //       </div>
+    //     </AppContext.Provider>
+    //   )
+    // }
+
     return (
-      <div className="App">
-       {/* <Navbar /> */}
-        <Switch>
-          <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
-          {/* <Route exact path="/projects" component={ProjectList}/>
-          <Route exact path="/projects/:id" component={ProjectDetails} /> */}
-        </Switch>
-      </div>
-    );
+      <AppContext.Provider value = {contextValues}>
+           <div className="App">
+           {/* <Navbar userInSession={this.state.loggedInUser} /> */}
+             <Switch>
+               <Route exact path='/signup' component={Signup}/>
+               <Route exact path='/auth/:confirmation' component={Confirmation}/>
+               {/* <Route exact path="/projects" component={ProjectList}/>
+               <Route exact path="/projects/:id" component={ProjectDetails} /> */}
+             </Switch>
+           </div>
+      </AppContext.Provider>
+    )
   }
 }
 
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -4,11 +4,19 @@ import React, { Component } from 'react';
 import AuthService from './auth-service';
 
 import { Link } from 'react-router-dom';
+import AppContext from '../../context/AppContext';
 
 class Signup extends Component {
   constructor(props){
     super(props);
-    this.state = { username: '', password: '' };
+    this.state = { 
+      cpf: '',
+      email: '',
+      celular: '',
+      nome: '',
+      // username: '',       
+      // password: '' 
+    };
     this.service = new AuthService();
   }
 
@@ -16,16 +24,25 @@ class Signup extends Component {
 
   handleFormSubmit = (event) => {
     event.preventDefault();
-    const username = this.state.username;
-    const password = this.state.password;
+    const cpf = this.state.cpf;
+    const email = this.state.email;
+    const celular = this.state.celular;
+    const nome = this.state.nome;
+    // const username = this.state.username;
+    // const password = this.state.password;
   
-    this.service.signup(username, password)
+    this.service.signup(cpf, email, celular, nome)
     .then( response => {
         this.setState({
-            username: "", 
-            password: "",
+            cpf: "",
+            email: "",
+            celular: "",
+            nome: "",
+            // username: "", 
+            // password: "",
         });
-        // this.props.getUser(response)
+        // console.log(response);
+        // this.context.getUser(response)
     })
     .catch( error => console.log(error) )
   }
@@ -37,24 +54,116 @@ class Signup extends Component {
 
   render(){
     return(
-        <div>
-        <form onSubmit={this.handleFormSubmit}>
-          <label>Username:</label>
-          <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
+      <AppContext.Consumer>
+        { context => (
+          <div className="container">
+            <div className="level">
+
+            <div className="level-item">
+            
+              <form onSubmit={this.handleFormSubmit}>
+                {/* <label>Username:</label> */}
+
+                <div className="field">
+
+                  <label className="label">CPF</label>
+
+                    <div className="control">
+                      <input className="input" type="text" placeholder="xxx.xxx.xxx-xx" name="cpf" value={this.state.cpf} onChange={ e => this.handleChange(e)}/>
+                    </div>
+
+                </div>
+
+                {/* <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/> */}
+                
+                <div className="field">
+
+                    <label className="label">Email</label>
+
+                      <div className="control has-icons-left has-icons-right">
+                        <input className="input" type="text" placeholder="email@email.com" name="email" value={this.state.email} onChange={ e => this.handleChange(e)}/>
+                        
+                        <span className="icon is-small is-left">
+                          <i className="fas fa-envelope"></i>
+                        </span>
+
+                        {/* <span class="icon is-small is-right">
+                          <i class="fas fa-exclamation-triangle"></i>
+                        </span> */}
+
+                      </div>
+
+                    {/* <p class="help is-danger">This email is invalid</p> */}
+
+                </div>
+
+                <div className="field">
+
+                    <label className="label">Celular</label>
+
+                    <div className="control has-icons-left has-icons-right">
+
+                      <input className="input" type="text" placeholder="(11) 9xxxx xxxx" name="celular" value={this.state.celular} onChange={ e => this.handleChange(e)}/>
+
+                        <span className="icon is-small is-left">
+                          <i className="fas fa-envelope"></i>
+                        </span>
+
+                        {/* <span class="icon is-small is-right">
+                          <i class="fas fa-exclamation-triangle"></i>
+                        </span> */}
+                    </div>
+
+                    {/* <p class="help is-danger">This email is invalid</p> */}
+                </div>
+
+                {/* <label>Password:</label>
+                <input name="password" value={this.state.password} onChange={ e => this.handleChange(e)} /> */}
+                
+                <div className="field">
+
+                    <label className="label">Nome</label>
+
+                    <div className="control has-icons-left has-icons-right">
+
+                      <input className="input" type="text" placeholder="Quebrado de tal" name="nome" value={this.state.nome} onChange={ e => this.handleChange(e)}/>
+
+                        <span className="icon is-small is-left">
+                          <i className="fas fa-envelope"></i>
+                        </span>
+
+                        {/* <span class="icon is-small is-right">
+                          <i class="fas fa-exclamation-triangle"></i>
+                        </span> */}
+                    </div>
+
+                    {/* <p class="help is-danger">This email is invalid</p> */}
+                </div>
+
+                {/* <input type="submit" value="Signup" /> */}
+
+               
+                <div className="control">
+                  <input type='submit' className="button is-link  is-fullwidth"/>
+                </div>
+                
+
+                <p>Already have account? 
+                  <Link to={"/"}> Login</Link>
+              </p>
+              </form>
+        
+              
+
+            </div>
           
-          <label>Password:</label>
-          <input name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
-          
-          <input type="submit" value="Signup" />
-        </form>
-  
-        <p>Already have account? 
-            <Link to={"/"}> Login</Link>
-        </p>
-  
-      </div>
-    )
+          </div>
+        </div>
+        )}
+      </AppContext.Consumer>
+    );
   }
 }
 
+Signup.contextType = AppContext;
 export default Signup;
