54b859e33513c9d67c2f874193276b89c546ad92
Jeferson Silva <jeferson.s.silva1@gmail.com>
2020-03-04 21:40:03 -0300

    ajustado footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -24,10 +24,6 @@ class Footer extends Component{
               <img className='monitor' src='https://mutual.club/assets/images/logo_monitor.png'/>
               <img className='estadao' src='https://mutual.club/assets/images/logo_estadao.png'/>
             </div>
-            <div class="map">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21955.962853684083!2d-46.66231942658338!3d-23.56017548991528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x694f27fed85d9b0c!2sIronhack%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1574213229841!5m2!1spt-BR!2sbr">
-              </iframe>
-            </div>
           </div>
         </footer>
       </React.Fragment>
